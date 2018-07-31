31b91fcb1293660cda48338bbc51edf682031d0b
Alex V. Surnin <info@volumnet.ru>
2018-07-31 15:36:56 +0500

    .

--- classes/forms-n-tables/userorderstable.class.php
+++ classes/forms-n-tables/userorderstable.class.php
@@ -8,7 +8,6 @@ class UserOrdersTable extends OrdersTable
     public function __construct(array $params = array())
     {
         parent::__construct($params);
-        // 2016-03-11, AVS: убрал удаление контекстного меню — непонятно, зачем удалял, клиентам неудобно админить отзывы
-        unset($this->Pages/*, $this->columns[' ']*/);
+        unset($this->Pages);
     }
 }
